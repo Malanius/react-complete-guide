21372dc27f57012a243f13b004f5832cf5062f20
Malanius <malanius@seznam.cz>
2018-07-12 20:00:46 +0200

    JSX code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>Hello from REACT!</h1>
+      <div className="App">
+        <h1>Hello from REACT!</h1>
+      </div>
+      //This is compiled to this:
+      //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello from REACT!!!'))
+
+      //Component has to have only one root element!
+      //<p>This wont work!<p>
     );
   }
 }
