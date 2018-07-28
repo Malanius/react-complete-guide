7f7621a528f10a2c146acd3d14a9560f852b1c56
Malanius <malanius@seznam.cz>
2018-07-28 14:00:35 +0200

    Styling components with inline styles

--- src/App.js
+++ src/App.js
@@ -37,10 +37,21 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hello from REACT!</h1>
-        <button onClick={() => this.switchNameHandler('Maxmillian!!')}>Switch name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maxmillian!!')}>
+          Switch name
+          </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
