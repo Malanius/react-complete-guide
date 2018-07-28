db7d1dda3b6f239faed59f9991728e0be0ee9d36
Malanius <malanius@seznam.cz>
2018-07-28 13:28:17 +0200

    Passing function reference

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
     otherState: 'some other value' //will be untouched by setState
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Clicked!');
     //this.state.persons[0].name = 'Maxmilinan' - do not change state directly, won't work
     this.setState({
       persons: [
-        { name: 'Maxmilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Melanie', age: 22 }
       ]
@@ -29,10 +29,19 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello from REACT!</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Gardening</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={ () => this.switchNameHandler('Maxmillian!!')}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}>
+          Hobbies: Gardening
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
       //This is compiled to this:
       //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello from REACT!!!'))
