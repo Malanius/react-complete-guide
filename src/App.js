46d0d886c789de3a91659b8a0e388959fd090414
Malanius <malanius@seznam.cz>
2018-07-28 11:08:27 +0200

    Use first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello from REACT!</h1>
+        <Person />
       </div>
       //This is compiled to this:
       //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello from REACT!!!'))
