fe91ae7f678e5d55ec14e0d792bcac9313c1b55c
Malanius <malanius@seznam.cz>
2018-08-05 15:48:43 +0200

    Reference in React 16.3+

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,6 +9,7 @@ class Person extends Component {
   constructor(props) {
     super(props); //Always call super constructor
     console.log('[Person.js] - Inside constructor', props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -18,7 +19,7 @@ class Person extends Component {
   componentDidMount() {
     console.log('[Person.js] - Inside componentDidMount()');
     if (this.props.position === 0) {
-      this.inputElement.focus();
+      this.inputElement.current.focus();
     }
   }
 
@@ -28,7 +29,7 @@ class Person extends Component {
         <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
         <p>{this.props.children}</p>
         <input
-          ref={(inp) => { this.inputElement = inp }}
+          ref={this.inputElement}
           type="text"
           onChange={this.props.changed}
           value={this.props.name} />
