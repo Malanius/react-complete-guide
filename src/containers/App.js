59088805de6f27a77b1cea9bfceeb0ac6acbe092
Malanius <malanius@seznam.cz>
2018-08-05 12:00:31 +0200

    Add logging to component internal update life cycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,8 +25,21 @@ class App extends Component {
     console.log('[App.js] - Inside componentWillMount()');
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log('[App.js] - Inside componentDidMount()');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] - Inside shouldComponentUpdate()', nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] - Inside componentWillUpdate()', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] - Inside componentDidUpdate()');
   }
 
   // This will work only with ES6 enabled
