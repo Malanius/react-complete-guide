f7c2ced2a3c019e6dc8cecd9d5d1b6ed066666dc
Malanius <malanius@seznam.cz>
2018-08-05 11:53:35 +0200

    Add logging to component update life cycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -6,15 +6,34 @@ class Persons extends Component {
     constructor(props) {
         super(props); //Always call super constructor
         console.log('[Persons.js] - Inside constructor', props);
-      }
-    
-      componentWillMount() {
+    }
+
+    componentWillMount() {
         console.log('[Persons.js] - Inside componentWillMount()');
-      }
-    
-      componentDidMount(){
+    }
+
+    componentDidMount() {
         console.log('[Persons.js] - Inside componentDidMount()');
-      }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('[UPDATE Persons.js] - Inside componentWillReceiveProps()', nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] - Inside shouldComponentUpdate()', nextProps, nextState);
+        //return true; //Return based on check of nextProps and nextState
+        //return false; //This will stop the update process
+        return nextProps !== this.props;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] - Inside componentWillUpdate()', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('[UPDATE Persons.js] - Inside componentDidUpdate()');
+    }
 
     render() {
         console.log('[App.js] - Inside render()');
