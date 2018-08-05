9ed057abc9ba6bf34585cbb1637afd97fd857622
Malanius <malanius@seznam.cz>
2018-08-05 17:32:56 +0200

    New lifecycle methods

--- src/containers/App.js
+++ src/containers/App.js
@@ -50,6 +50,15 @@ class App extends PureComponent {
     console.log('[UPDATE App.js] - Inside componentDidUpdate()');
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('[UPDATE App.js] - Inside getDerivedStateFromProps()', nextProps, prevState);
+    return prevState;
+  }
+
+  getSnapshotBeforeUpdate(){
+    console.log('[UPDATE App.js] - Inside getSnapshotBeforeUpdate()');
+  }
+
   // This will work only with ES6 enabled
   // state = {
   //   persons: [
